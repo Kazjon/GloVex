205c249b852af62c7a194cedad922c7689fc7d89
kazjon <kazjon@me.com>
2017-04-28 10:52:38 -0400

    Made surprise ignore feature/context order for now.  This might have an impact on the distance calculation, need to look into that.

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,4 @@
-import csv,gensim,logging,sys,os.path,multiprocessing, nltk, io, glove, itertools, pprint, argparse, scipy.spatial, glob
+import csv,gensim,logging,sys,os.path,multiprocessing, nltk, io, glove, argparse, glob
 import cPickle as pickle
 import numpy as np
 from nltk.corpus import stopwords,wordnet
@@ -7,6 +7,7 @@ from nltk.tokenize import RegexpTokenizer
 from joblib import Parallel, delayed
 from inflection import singularize
 from prettytable import PrettyTable
+import evaluate
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger("glovex")
@@ -102,89 +103,7 @@ class ACMDL_DocReader(object):
 			for wk,wv in self.dictionary.iteritems():
 				self.cooccurrence[wk] = {wk2:float(wv2)/self.word_occurrence[wv] for wk2,wv2 in self.cooccurrence[wk].iteritems()}
 
-def word_pair_surprise(w1_w2_cooccurrence, w1_occurrence, w2_occurrence, n_docs, offset = 1):
-	# Offset is Laplacian smoothing
-	w1_w2_cooccurrence = min(min(w1_occurrence,w2_occurrence),max(0,w1_w2_cooccurrence)) #Capped due to estimates being off sometimes
-	p_w1_given_w2 = (w1_w2_cooccurrence + offset) / (w2_occurrence + offset)
-	p_w1 = (w1_occurrence + offset) / (n_docs + offset)
-	return p_w1_given_w2 / p_w1
 
-def extract_document_cooccurrence_matrix(doc, coocurrence):
-	cooc_mat = np.zeros([len(doc),len(doc)])
-	for i1,i2 in itertools.combinations(range(len(doc)),2):
-		d1 = doc[i1][0]
-		d2 = doc[i2][0]
-		cooc_mat[i1,i2] = coocurrence[d1][d2]
-	return cooc_mat
-
-def estimate_word_pair_cooccurrence(wk1, wk2, model, cooccurrence):
-	# take dot product of vectors
-	cooc = np.dot(model.W[wk1],model.ContextW[wk2]) + model.b[wk1] + model.ContextB[wk2]
-	# correct for the rare feature scaling described in https://nlp.stanford.edu/pubs/glove.pdf
-	if cooccurrence[wk1][wk2] < model.x_max:
-		cooc *= 1.0/pow(cooccurrence[wk1][wk2] / model.x_max,model.alpha)
-	return cooc[0]
-
-def estimate_document_cooccurrence_matrix(doc, model, cooccurrence):
-	cooc_mat = np.zeros([len(doc),len(doc)])
-	for i1,i2 in itertools.combinations(range(len(doc)),2):
-		d1 = doc[i1][0]
-		d2 = doc[i2][0]
-		cooc_mat[i1,i2] = estimate_word_pair_cooccurrence(d1, d2, model, cooccurrence)
-	return np.triu(np.exp(cooc_mat), k=1)
-
-def top_n_surps_from_doc(doc, model, cooccurrence, word_occurrence, dictionary, n_docs, top_n = 10):
-	if len(doc):
-		est_cooc_mat = estimate_document_cooccurrence_matrix(doc,model,cooccurrence)
-		surps = document_cooccurrence_to_surprise(doc, est_cooc_mat, word_occurrence, dictionary, n_docs)
-		surps.sort(key = lambda x: x[2], reverse=False)
-		return surps[:min(top_n,len(surps))]
-	else:
-		return []
-
-#Returns an ordered list of most-to-least surprising word combinations as (w1,w2,surprise) tuples
-def document_cooccurrence_to_surprise(doc, cooc_mat, word_occurrence, dictionary, n_docs):
-	surp_list = []
-	it = np.nditer(cooc_mat+cooc_mat.T, flags=['multi_index'])
-	while not it.finished:
-		w1 = doc[it.multi_index[0]][0]
-		w2 = doc[it.multi_index[1]][0]
-		if not w1 == w2:
-			s = word_pair_surprise(it[0], word_occurrence[dictionary[w1]], word_occurrence[dictionary[w2]], n_docs)
-			surp_list.append((dictionary[w1], dictionary[w2],s))
-		it.iternext()
-	surp_list.sort(key = lambda x: x[2], reverse=False)
-	return surp_list
-
-#Return the surprises from the given list that have the most similar feature word (i.e. w1) to the one in the given surp.
-def most_similar_features(surp, surp_list, model, n = 10):
-	results = []
-	for surp2 in surp_list:
-		surp_feat = model.W[surp[0]]
-		surp2_feat = model.W[surp2[0]]
-		results.append([surp,surp2,scipy.spatial.distance.euclidean(surp_feat, surp2_feat)])
-	results.sort(key = lambda x: x[2])
-	return results[:n]
-
-#Return the surprises from the given list that have the most similar context word(s) (i.e. w2) to the one in the given surp.
-def most_similar_contexts(surp, surp_list, model, n = 10):
-	results = []
-	for surp2 in surp_list:
-		surp_context = model.W[surp[1]]
-		surp2_context = model.W[surp2[1]]
-		results.append([surp,surp2,scipy.spatial.distance.euclidean(surp_context, surp2_context)])
-	results.sort(key = lambda x: x[2])
-	return results[:n]
-
-#Return the surprises from the given list that have the most similar vector difference to the one in the given surp.
-def most_similar_differences(surp, surp_list, model, n = 10):
-	results = []
-	for surp2 in surp_list:
-		surp_diff = model.W[surp[0]] -  model.W[surp[1]]
-		surp2_diff = model.W[surp2[0]] -  model.W[surp2[1]]
-		results.append([surp,surp2,surp_diff,surp2_diff,scipy.spatial.distance.euclidean(surp_diff, surp2_diff)])
-	results.sort(key = lambda x: x[4])
-	return results[:n]
 
 def glovex_model(filepath, argstring, cooccurrence, dims=100, alpha=0.75, x_max=100, force_overwrite = False, suffix = ".glovex"):
 	model_path = filepath+argstring
@@ -202,17 +121,6 @@ def save_model(model,path,args,suffix=".glovex"):
 	with open(path+args+suffix,"wb") as f:
 		pickle.dump(model,f)
 
-def estimate_document_surprise_pairs(doc, model, acm, top_n_per_doc = 0):
-	est_cooc_mat = estimate_document_cooccurrence_matrix(doc,model,acm.cooccurrence)
-	surps = document_cooccurrence_to_surprise(doc, est_cooc_mat, acm.word_occurrence, acm.dictionary, len(acm.documents))
-	surps.sort(key = lambda x: x[2])
-	if top_n_per_doc:
-		return surps[:top_n_per_doc]
-	return surps
-
-def percentile_doc_surprise(doc, model, acm, percentile = 95):
-	surps = estimate_document_surprise_pairs(doc, model, acm)
-	return np.percentile([x[2] for x in surps], percentile)
 
 def print_top_n_surps(model, acm):
 	#Parallelised version -- takes forever, not sure why, leaving this for now
@@ -229,7 +137,7 @@ def print_top_n_surps(model, acm):
 	top_surps = []
 	for doc in acm.documents:
 		if len(doc):
-			top_surps += estimate_document_surprise_pairs(doc, model, acm)[:10]
+			top_surps += evaluate.estimate_document_surprise_pairs(doc, model, acm)[:10]
 			top_surps = list(set(top_surps))
 			top_surps.sort(key = lambda x: x[2], reverse=False)
 			top_surps = top_surps[:10]
@@ -253,10 +161,10 @@ def print_top_n_surps(model, acm):
 		est_surps.append(surp[2])
 		wk1 = acm.dictionary.token2id[surp[0]]
 		wk2 = acm.dictionary.token2id[surp[1]]
-		est_coocs.append(estimate_word_pair_cooccurrence(wk1, wk2, model, acm.cooccurrence))
+		est_coocs.append(evaluate.estimate_word_pair_cooccurrence(wk1, wk2, model, acm.cooccurrence))
 		w1_w2_cooccurrence = acm.cooccurrence[wk1][wk2]
 		obs_coocs.append(w1_w2_cooccurrence)
-		obs_surp = word_pair_surprise(w1_w2_cooccurrence, w1_occ, w2_occ, len(acm.documents))
+		obs_surp = evaluate.word_pair_surprise(w1_w2_cooccurrence, w1_occ, w2_occ, len(acm.documents))
 		obs_surps.append(obs_surp)
 
 	tab = PrettyTable()

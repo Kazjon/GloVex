ef8fbf00d597a19a8d1ecf64e29f44ac42fc792d
Kazjon <k.grace@uncc.edu>
2018-01-12 14:11:18 +1100

    Fam_cat simplification redo (#3)
    * Adding Recipe_Reader to the preprocessor.py
    
    * Remove unnecessary prints
    
    * Just making sure that I am committing the correct version
    
    * Input fam_cats using another file in a dict format
    
    * Simplify the parsing of fam_cat (use the same data file)

--- preprocessor.py
+++ preprocessor.py
@@ -14,9 +14,11 @@ import random
 import fisher
 import itertools
 
+# Logging info from Glovex messages
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger("glovex")
 
+# Get part of speach tags (Adjectives, Verbs, Nouns and Adverbs)
 def get_wordnet_pos(treebank_tag):
 	if treebank_tag.startswith('J'):
 		return wordnet.ADJ
@@ -53,6 +55,7 @@ def significance_on_tuple(sig_tuple):
 def significance_on_tuple_batch(sig_tuple_batch):
 	return [significance_on_tuple(sig_tuple) for sig_tuple in sig_tuple_batch]
 
+#Document reader class
 class DocReader(object):
 	def __init__(self,path,famcat_path, run_name=None, use_sglove=False):
 		self.filepath = path
@@ -76,15 +79,18 @@ class DocReader(object):
 		self.cooccurrence_p_values = {}
 		self.use_sglove = use_sglove
 
+	# Document reader iterator not implemented
 	def __iter__(self):
 		raise NotImplementedError
 
+	# Load function (using pickle) for document reader
 	def load(self,preprocessed_path):
 		with open(preprocessed_path,"rb") as pro_f:
 			self.documents,self.word_occurrence, self.cooccurrence,self.dictionary, self.total_docs, self.doc_ids, self.doc_titles, self.doc_raws, self.doc_famcats, self.per_fc_keys_to_all_keys, self.all_keys_to_per_fc_keys, self.docs_per_fc, self.cooccurrence_p_values, self.use_sglove = pickle.load(pro_f)
 			self.famcats = self.cooccurrence.keys()
 			self.first_pass = False
 
+	# Preprocessing function of the Document reader
 	def preprocess(self,suffix=".preprocessed", no_below=0.001, no_above=0.5, force_overwrite = False):
 		if self.run_name is not None:
 			self.argstring = "_"+self.run_name+"_below"+str(no_below)+"_above"+str(no_above)
@@ -112,6 +118,7 @@ class DocReader(object):
 			self.load(preprocessed_path)
 		logger.info(" ** Pre-processing complete.")
 
+	# Calculate cooccurrence function of the Document reader
 	#Note: Normalisation not implemented for personalised version (w/ famcats)
 	def calc_cooccurrence(self, normalise = False):
 		if self.famcat_filepath is None:
@@ -236,6 +243,7 @@ class DocReader(object):
 				#print sig, p
 				self.cooccurrence_p_values[sig[0]][sig[1]] = p
 
+# ACMDL Document reader which is a subclass of the Document reader
 class ACMDL_DocReader(DocReader):
 	def __init__(self,path, title_column, text_column, id_column, famcat_path=None, run_name=None, use_sglove=False):
 		self.title_column = title_column
@@ -243,15 +251,16 @@ class ACMDL_DocReader(DocReader):
 		self.id_column = id_column
 		DocReader.__init__(self,path,famcat_path, run_name=run_name, use_sglove=use_sglove)
 
+	# The iterator of the ACMDL Document reader
 	def __iter__(self):
 		if self.first_pass and self.famcat_filepath is not None:
 			with io.open(self.famcat_filepath+".csv",mode="r",encoding='ascii',errors="ignore") as famcat_file:
 				reader = csv.reader(famcat_file)
-				#famcats = {row[0]:(row[1:] if len(row) > 1 else []) for row in reader}
+				famcats = {row[0]:(row[1:] if len(row) > 1 else []) for row in reader}
 
-				#Hacks for working with fake author-based famcats
-				famcats = {row[0]:([n[0] for n in row[1:] if len(n)] if len(row) > 1 else ["None"]) for row in reader}
-				#famcats = {row[0]:["1"] if random.random() > 0.5 else ["1","2"] for row in reader}
+				# Hacks for working with fake author-based famcats
+				# famcats = {row[0]:([n[0] for n in row[1:] if len(n)] if len(row) > 1 else ["None"]) for row in reader}
+				# famcats = {row[0]:["1"] if random.random() > 0.5 else ["1","2"] for row in reader}
 		with io.open(self.filepath+".csv",mode="r",encoding='ascii',errors="ignore") as i_f:
 			for row in csv.DictReader(i_f):
 				docwords = [singularize(w) for w in self.tokeniser.tokenize((row[self.title_column]+" "+row[self.text_column]).lower()) if w not in self.stop]
@@ -270,10 +279,12 @@ class ACMDL_DocReader(DocReader):
 				yield docwords
 		self.first_pass = False
 
+# WikiPlot Document reader class
 class WikiPlot_DocReader(DocReader):
 	def __init__(self,path):
 		DocReader.__init__(self,path)
 
+	# Iterator of the WikiPlot Document reader
 	def __iter__(self):
 		with io.open(self.filepath,mode="r",encoding='ascii',errors="ignore") as i_f:
 			if self.first_pass:
@@ -299,60 +310,62 @@ class WikiPlot_DocReader(DocReader):
 			t_f.close()
 		self.first_pass = False
 
+# Recipe Document reader
 class Recipe_Reader(DocReader):
 	def __init__(self,path, text_column, id_column, famcat_path=None):
 		self.text_column = text_column
 		self.id_column = id_column
+    self.fam_cat_column = 'cuisine'
 		DocReader.__init__(self,path,famcat_path)
 
+	# The iterator of the Recipe Document reader
 	def __iter__(self):
-		if self.first_pass and self.famcat_filepath is not None:
-			with io.open(self.famcat_filepath+".csv",mode="r",encoding='ascii',errors="ignore") as famcat_file:
-				reader = csv.reader(famcat_file)
-				#famcats = {row[0]:(row[1:] if len(row) > 1 else []) for row in reader}
-
-				#Hacks for working with fake author-based famcats
-				famcats = {row[0]:([n[0] for n in row[1:] if len(n)] if len(row) > 1 else ["None"]) for row in reader}
-				#famcats = {row[0]:["1"] if random.random() > 0.5 else ["1","2"] for row in reader}
 		with io.open(self.filepath + ".csv", mode="r", encoding='ascii', errors="ignore") as i_f:
 			for row in csv.DictReader(i_f):
 				docwords = [singularize(w) for w in self.tokeniser.tokenize((row[self.text_column]).lower()) if
 							w not in self.stop]
-				# If no frist pass, get the document IDs, text_column and famcats (if the famcat_filepath is not None)
+				# If not first pass, get the document IDs, text_column and famcats (if the famcat_filepath is not None)
 				if self.first_pass:
 					self.doc_ids.append(row[self.id_column])
 					self.doc_raws.append(row[self.text_column])
 					if self.famcat_filepath is not None:
-						self.doc_famcats.append(famcats[row[self.id_column]])
+						self.doc_famcats.append(row[self.fam_cat_column])
 				yield docwords
 		self.first_pass = False
 
-def glovex_model(filepath, argstring, cooccurrence, p_values, dims=100, alpha=0.75, x_max=100, force_overwrite = False, suffix = ".glovex", use_sglove=False):
-	model_path = filepath+argstring
+# Glovex model builder
+def glovex_model(filepath, argstring, cooccurrence, dims=100, alpha=0.75, x_max=100, force_overwrite = False, suffix = ".glovex", use_sglove=False, p_values=None):
+	# Get all file names with .glovex extension in the model's path
+  model_path = filepath+argstring
 	model_files = glob.glob(model_path+"_epochs*"+suffix)
 	if not len(model_files) or force_overwrite:
+    # If no model exists or it is forced to overwrite the old model, create a new model
 		if use_sglove:
 			model = s_glove.Glove(cooccurrence, p_values, d=dims, alpha=alpha)
 		else:
 			model = glove.Glove(cooccurrence, d=dims, alpha=alpha, x_max=x_max)
 	else:
-		highest_epochs = max([int(f.split("epochs")[1].split(".")[0]) for f in model_files])
+  	# If a model exists and no overwrite is forced, use the existing model at its last trained epoch	
+    highest_epochs = max([int(f.split("epochs")[1].split(".")[0]) for f in model_files])
 		logger.info(" ** Existing model file found.  Re-run with --overwrite_model if you did not intend to reuse it.")
 		with open(model_path+"_epochs"+str(highest_epochs)+suffix,"rb") as pro_f:
 			model = pickle.load(pro_f)
 	return model
 
+# Save the Glovex model function (with a .glovex extension)
 def save_model(model,path,args,suffix=".glovex"):
 	with open(path+args+suffix,"wb") as f:
 		pickle.dump(model,f)
 
+# Load the personalised model function
 def load_personalised_models(filepath, docreader):
 	models = []
 	for fc in docreader.famcats:
 		models.append(glovex_model(filepath, docreader.argstring+"_fc"+str(fc), docreader.cooccurrence[fc]))
 	return models
 
-def print_top_n_surps(model, acm):
+# Print top n surprise scores function
+def print_top_n_surps(model, acm, top_n):
 
 	top_surps = []
 	for doc in acm.documents:
@@ -360,9 +373,9 @@ def print_top_n_surps(model, acm):
 			top_surps += evaluate.estimate_document_surprise_pairs(doc, model, acm)[:10]
 			top_surps = list(set(top_surps))
 			top_surps.sort(key = lambda x: x[2], reverse=False)
-			top_surps = top_surps[:10]
+			top_surps = top_surps[:top_n]
 
-	print "Top 10 surprising combos"
+	print "top_n surprising combos"
 	w1s = []
 	w2s = []
 	w1_occs = []
@@ -399,8 +412,9 @@ def print_top_n_surps(model, acm):
 	tab.float_format = ".4"
 	print tab
 
-
+# Main function
 if __name__ == "__main__":
+	# Parse arguments from the command
 	parser = argparse.ArgumentParser(description="Run GloVeX on some text.")
 	parser.add_argument("inputfile", help='The file path to work with (omit the ".csv")')
 	parser.add_argument("--dataset", default="acm",type=str, help="Which dataset to assume.  Currently 'acm' or 'plots'")
@@ -424,8 +438,9 @@ if __name__ == "__main__":
 						help="Use the modified version of the GloVe algorithm that favours surprise rather than co-occurrence.")
 	parser.add_argument("--familiarity_categories", default=None, type=str,
 						help='The (optional) path to the file containing IDs and familiarity categories (omit the ".csv")')
-
 	args = parser.parse_args()
+
+	# Read the documents according to its type
 	if args.dataset == "acm":
 		reader = ACMDL_DocReader(args.inputfile, "title", "abstract", "ID", famcat_path=args.familiarity_categories, run_name=args.name, use_sglove=args.use_sglove)
 	elif args.dataset == "plots":
@@ -435,30 +450,42 @@ if __name__ == "__main__":
 	else:
 		logger.info("You've tried to load a dataset we don't know about.  Sorry.")
 		sys.exit()
+
+	# Preprocess the data
 	reader.preprocess(no_below=args.no_below, no_above=args.no_above, force_overwrite=args.overwrite_preprocessing)
+	
 	init_step_size = args.learning_rate
 	step_size_decay = 25.0
 	cores = multiprocessing.cpu_count() / 2
+
+  # If the familiarity categories (fam_cat) are unknown
 	if args.familiarity_categories is None:
-		model = glovex_model(args.inputfile, reader.argstring, reader.cooccurrence, reader.cooccurrence_p_values, args.dims, args.glove_alpha, args.glove_x_max,
-							 args.overwrite_model, use_sglove=args.use_sglove)
+		model = glovex_model(args.inputfile, reader.argstring, reader.cooccurrence, args.dims, args.glove_alpha, args.glove_x_max,
+							 args.overwrite_model, use_sglove=args.use_sglove, p_values=reader.cooccurrence_p_values)
 		logger.info(" ** Training GloVe")
 		for epoch in range(args.epochs):
 			err = model.train(workers=cores, batch_size=100, step_size=init_step_size/(1.0+epoch/step_size_decay))
 			logger.info("   **** Training GloVe: epoch %d, error %.5f" % (epoch, err))
+      
 			if epoch and epoch % args.print_surprise_every == 0:
-				print_top_n_surps(model, reader)
+				top_n = 50
+				print_top_n_surps(model, reader, top_n)
 				save_model(model, args.inputfile, reader.argstring+"_epochs"+str(epoch))
 		save_model(model, args.inputfile, reader.argstring+"_epochs"+str(epoch))
+
+  # If the familiarity categories (fam_cat) are known
 	else:
 		for fc,fc_cooccurrence in reader.cooccurrence.iteritems():
-			model = glovex_model(args.inputfile, reader.argstring+"_fc"+fc, fc_cooccurrence, reader.cooccurrence_p_values[fc], args.dims, args.glove_alpha, args.glove_x_max,
-								 args.overwrite_model, use_sglove=args.use_sglove)
+			# Pass the familiarity category (fam_cat) file to the glovex_model function
+      model = glovex_model(args.inputfile, reader.argstring+"_fc"+fc, fc_cooccurrence, args.dims, args.glove_alpha, args.glove_x_max,
+								 args.overwrite_model, use_sglove=args.use_sglove, p_values=reader.cooccurrence_p_values[fc])
+
 			logger.info(" ** Training GloVe for "+fc)
 			for epoch in range(args.epochs):
 				err = model.train(workers=cores, batch_size=100, step_size=init_step_size/(1.0+epoch/step_size_decay))
 				logger.info("   **** Training GloVe for "+fc+": epoch %d, error %.5f" % (epoch, err))
 				if epoch and epoch % args.print_surprise_every == 0:
-					print_top_n_surps(model, reader)
+          top_n = 50
+					print_top_n_surps(model, reader, top_n)
 					save_model(model, args.inputfile, reader.argstring+"_epochs"+str(epoch))
 			save_model(model, args.inputfile, reader.argstring+"_epochs"+str(epoch))
